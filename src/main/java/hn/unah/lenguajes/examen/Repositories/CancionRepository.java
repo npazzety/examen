6760c7369110289354ecbd33fbf4831095b2d2fb
npazzety <nelsonpazzety10@gmail.com>
2024-04-05 14:39:08 -0600

    definitivo

--- src/main/java/hn/unah/lenguajes/examen/Repositories/CancionRepository.java
+++
@@ -1,9 +0,0 @@
-package hn.unah.lenguajes.examen.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import hn.unah.lenguajes.examen.Modelos.Cancion;
-
-public interface CancionRepository extends CrudRepository<Cancion,String> {
-    
-}
